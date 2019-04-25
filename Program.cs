b863e30af9557289affe7662ea921f80d58adaba
Noe <VaescoNoe@gmail.com>
2019-04-25 00:01:37 -0500

    For doble variable

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Procesos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int  i = 0, j = 10;  i <=  j;  i++ , j--)
+            {
+                Console.WriteLine($"i={i}");
+                Console.WriteLine($"j={j}");
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("infinito");
+            }
+
+        }
+    }
+}
